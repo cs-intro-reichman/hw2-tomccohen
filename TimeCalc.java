f174f0390462f08b4170c87c9a0c94576c159ac4
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-05 17:56:16 +0200

    TimeCalc implementation

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,8 +4,8 @@ public class TimeCalc {
         int min2add = Integer.parseInt(args[1]);    //the input of the min to add    
         String hoursStr = "" + args[0].charAt(0) + args[0].charAt(1);
         String minStr = "" + args[0].charAt(3) + args[0].charAt(4);   
-        int hours = Integer.parseInt(hoursStr);
-        int minutes = Integer.parseInt(minStr);
+        int hours = Integer.parseInt(hoursStr); //converting String to integer
+        int minutes = Integer.parseInt(minStr); //converting String to integer
         int totalminutes = (hours * 60) + minutes + min2add; 
         int totalhours = totalminutes / 60; // How much hours was added
         int newhours = totalhours % 24; // the new time "hh"
@@ -26,17 +26,5 @@ public class TimeCalc {
         {
             System.out.println(newhours + ":" + newminutes);
         }
-            
-        
-
-
-
-        //System.out.println(newhours + ":" + newminutes); 1380 23
-
-        //System.out.println("hours: " + hours);  
-        //System.out.println("minutes: " + minutes);  
-        //System.out.println("minutes to add: " + min2add);
-        //System.out.println("new hours : " + newhours);
-        //System.out.println("new minutes : " + newminutes);
     }
 }
