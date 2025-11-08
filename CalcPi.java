a84d7e5bc906fec278ab7a59c9738cb0a5ff3eb7
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-08 14:10:23 +0200

    CalcPi implemetation

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		double sum = 0;
+		for (int i = 0; i < num; i++){
+			double fraction = (1.0 / (2 * i + 1));
+			if (i % 2 == 0){
+				sum += fraction;
+			} else {
+				sum -= fraction;
+			}
+		}
+		double pi = Math.PI;
+		System.out.println("pi according to JAVA: " + pi);
+		System.out.println("pi, approximated:     " + sum * 4);
+		}
 	}
-}
