b80a99e62dda6470b029ef67d7780ccc153af37d
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-08 14:12:12 +0200

    CalcPi implemetation

--- CalcPi.java
+++ CalcPi.java
@@ -3,12 +3,14 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int num = Integer.parseInt(args[0]);
 		double sum = 0;
+
 		for (int i = 0; i < num; i++){
 			double fraction = (1.0 / (2 * i + 1));
+			
 			if (i % 2 == 0){
-				sum += fraction;
+				sum += fraction; //calculate the sum of odds
 			} else {
-				sum -= fraction;
+				sum -= fraction; //calculate the sum  of evens
 			}
 		}
 		double pi = Math.PI;
