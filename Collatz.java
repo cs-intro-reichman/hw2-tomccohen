c54bf815480e10d1eae8e3bc260cdade5f7cefbb
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-10 13:52:11 +0200

    Collatz implementation

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,60 @@
-// Demonstrates the Collatz conjecture.
+	// Demonstrates the Collatz conjecture.
+
+import javax.print.DocFlavor.STRING;
+
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+		public static void main(String args[]) {
+			int num = Integer.parseInt(args[0]);
+			String mode = args [1];
+			if (mode.equals("v")){
+
+			for (int line = 1; line <= num; line++ ){ 
+				int n = line;
+				int stepsTillOne = 1;
+				System.out.print(n + " ");
+					if (n % 2 == 1){
+						n = n * 3 + 1;
+					}else {
+						n = n / 2;
+					}
+					System.out.print(n + " ");
+					stepsTillOne++;
+					
+				while ( n > 1 ){
+					if (n % 2 == 0){
+					n = n / 2;
+					} else {
+					n = n * 3 + 1;
+					} 
+					System.out.print(n + " ");
+					stepsTillOne++;
+				}
+					System.out.print("(" + stepsTillOne + ")");
+					System.out.println();
+			}
+					System.out.println("Every one of the first " + num + " hailstone sequences reached 1");
+
+					
+		} if (mode.equals("c")) {
+
+			for (int line = 1; line <= num; line++ ){ 
+				int n = line;
+					if (n % 2 == 1){
+						n = n * 3 + 1;
+					}else {
+						n = n / 2;
+					}
+					
+				while ( n > 1 ){
+					if (n % 2 == 0){
+					n = n / 2;
+					} else {
+					n = n * 3 + 1;
+					} 
+				}
+			}
+					System.out.println("Every one of the first " + num + " hailstone sequences reached 1");
+		}
+		}
 	}
-}
+
