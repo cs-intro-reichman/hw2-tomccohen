1fb148ffb058d7df58090b45aefde5f8da598432
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-10 13:54:21 +0200

    Collatz implementation

--- Collatz.java
+++ Collatz.java
@@ -6,7 +6,8 @@ public class Collatz {
 		public static void main(String args[]) {
 			int num = Integer.parseInt(args[0]);
 			String mode = args [1];
-			if (mode.equals("v")){
+
+		if (mode.equals("v")){
 
 			for (int line = 1; line <= num; line++ ){ 
 				int n = line;
@@ -34,27 +35,27 @@ public class Collatz {
 			}
 					System.out.println("Every one of the first " + num + " hailstone sequences reached 1");
 
-					
+
 		} if (mode.equals("c")) {
 
-			for (int line = 1; line <= num; line++ ){ 
-				int n = line;
-					if (n % 2 == 1){
+				for (int line = 1; line <= num; line++ ){ 
+					int n = line;
+						if (n % 2 == 1){
 						n = n * 3 + 1;
-					}else {
-						n = n / 2;
-					}
+						}else {
+							n = n / 2;
+						}
 					
-				while ( n > 1 ){
-					if (n % 2 == 0){
-					n = n / 2;
-					} else {
-					n = n * 3 + 1;
-					} 
+					while ( n > 1 ){
+						if (n % 2 == 0){
+						n = n / 2;
+						} else {
+						n = n * 3 + 1;
+						} 
+					}
 				}
-			}
 					System.out.println("Every one of the first " + num + " hailstone sequences reached 1");
-		}
+			}
 		}
 	}
 
